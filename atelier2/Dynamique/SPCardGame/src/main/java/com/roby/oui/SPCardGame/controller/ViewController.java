9932228a4beb94dd02c4a508136b8cfefb59f3b4
anais <anais.delcamp@gmail.com>
2024-05-22 09:18:41 +0200

    rename RequestCrt.java in ViewController.java

--- atelier2/Dynamique/SPCardGame/src/main/java/com/roby/oui/SPCardGame/controller/ViewController.java
+++ atelier2/Dynamique/SPCardGame/src/main/java/com/roby/oui/SPCardGame/controller/ViewController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-public class RequestCrt {
+public class ViewController {
 
     @Autowired
     private CardDao cardDao;
