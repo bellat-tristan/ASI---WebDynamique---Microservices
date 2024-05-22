5559bde6b1984f375716f565e0fcdf6c967f5869
Davijea <116623825+Davijea@users.noreply.github.com>
2024-05-22 14:55:00 +0200

    test commit

--- atelier2/Dynamique/SPCardGame/src/main/java/com/roby/oui/SPCardGame/controller/CardDao.java
+++ atelier2/Dynamique/SPCardGame/src/main/java/com/roby/oui/SPCardGame/controller/CardDao.java
@@ -23,7 +23,7 @@ public class CardDao {
 
     private void createCardList() {
 
-        Card p1=new Card("John", "Test", "https://www.pokemon.com/static-assets/content-assets/cms2-fr-fr/img/cards/web/SVP/SVP_FR_47.png","Feu","affinity",23,34,15,29, 20);
+        Card p1=new Card("Joh", "Test", "https://www.pokemon.com/static-assets/content-assets/cms2-fr-fr/img/cards/web/SVP/SVP_FR_47.png","Feu","affinity",23,34,15,29, 20);
         Card p2=new Card("Roberto", "Test", "https://www.pokemon.com/static-assets/content-assets/cms2-fr-fr/img/cards/web/SVP/SVP_FR_47.png","Feu","affinity",23,34,15,29, 30);
         Card p3=new Card("Anna", "Test", "https://www.pokemon.com/static-assets/content-assets/cms2-fr-fr/img/cards/web/SVP/SVP_FR_47.png","Feu","affinity",23,34,15,29, 5);
         Card p4=new Card("Angry Joe","Test", "https://www.pokemon.com/static-assets/content-assets/cms2-fr-fr/img/cards/web/SVP/SVP_FR_47.png","Feu","affinity",23,34,15,29, 34);
